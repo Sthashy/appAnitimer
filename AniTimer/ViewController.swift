5f2a8fb860674a5648a42450060006e1c98237e0
johngbarreto <johngabrielbarreto1337@gmail.com>
2024-08-29 19:11:54 -0300

    Initial Commit

---
+++ AniTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AniTimer
+//
+//  Created by João Gabriel Lavareda Ayres Barreto on 29/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
